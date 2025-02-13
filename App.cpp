6461353ced51e8d3ce9134d0fe544ee6f8f9e955
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-02-13 09:26:32 +0500

    modifid that add two number

--- App.cpp
+++ App.cpp
@@ -108,3 +108,24 @@ int GetDigit(string s)
 	}
 	return digit;
 }
+
+using namespace std;
+
+int main() {
+    double num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Perform addition
+    sum = num1 + num2;
+
+    // Display the result
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
